ad7b966289c828d420fdef33779eebccd30f2dcd
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-09-21 19:39:22 +0200

    Created script to merge css files
    NOTE : In the future, this could also be used to merge other types of files, such as js files with generic utilities and such. For now, this is what we have.

---
+++ tool/merge.py
@@ -0,0 +1,13 @@
+# NOTE : The purpose of this program is to merge all of the css files into a single stylesheet file.
+# This is done so that the amount of requests that browsers must make is lowered.
+# This helps improve load times even when merging into a single large css file.
+
+# TODO : In the future, simplify the files themselves by parsing them and getting rid of all
+# non-significant whitespace, so as to reduce the amount of bytes sent through the network.
+
+def main():
+    # TODO : Implement
+    pass
+
+if __name__ == "__main__":
+    main()
